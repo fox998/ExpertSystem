d23a3d80584191d93a71be2a1d0d32a721f1fe25
afokin <andrii.fokin@ubisoft.com>
2019-09-30 16:39:14 +0300

    add start functional for resolve_queries

--- main.py
+++ main.py
@@ -2,51 +2,46 @@
 from parse_expert_data import ExpertData
 from parse_expert_data import parse_expert_data
 
-import statement
+from statement import init_form_initial_facts_arr
+from statement import init_from_implies_arr
+from statement import StatementMap
 
-def init_from_facts(initial_facts):
-    #is seams to '=ABC'?
-    print(initial_facts)
-    print(f'initial_facts[0] = {initial_facts[0]}')
-    if not isinstance(initial_facts, str) or initial_facts[0] != '=':
+
+def check_queries_format(queries):
+    if not isinstance(queries, str) or len(queries) < 2 or queries[0] != '?':
         raise Exception('wrong format of initial facts')
 
-    elif len(initial_facts) > 1:
-        for fact in initial_facts[1:]:
-            statement.Statements[fact] = statement.StatementValue(True)
+
+def resolve_statement(fact):
+    return False
 
 
-def init_form_initial_facts_arr(initial_facts_arr):
-    for initial_facts in initial_facts_arr:
-        init_from_facts(initial_facts)
+def resolve_facts(facts_arr):
+    for fact in facts_arr:
+        print(f'{fact} => {resolve_statement(fact)}')
 
 
-def fromate_implies(implies):
-    implies_parts = str(implies).partition('=>')
-    #is implies seams to 'C => E'?
-    if not isinstance(implies, str) or len(implies_parts) != 3:
-        raise Exception('wrong format of implies')
-    elif len(implies_parts[2]) == 0 or len(implies_parts[0]) == 0:
-        raise Exception('some part of implies is empty')
-    elif '<' in implies_parts[0]:
-        raise Exception('does not work with biconditional rules')
+def resolve_queries(queries_arr):
+    for queries in queries_arr:
+        check_queries_format(queries)
+        # queries ~ '?GVX'
+        # first char = '?', other chars are facts ~ 'GVX'
+        resolve_facts(queries[1:])
 
-    return implies_parts
-
-def init_from_implies_arr(implies_arr):
-    for implies in implies_arr:
-        implies_parts = fromate_implies(implies)
-        # implies_parts ~ {'C', '=>', 'E'}. 'E' became a key and 'C' become a value
-        statement.Statements[implies_parts[2]] = statement.StatementValue(implies_parts[0])
-
+    print(queries_arr)
 
 
 if __name__ == "__main__":
     expert_data = parse_expert_data("input.txt")
+
     init_form_initial_facts_arr(expert_data.initial_facts)
     init_from_implies_arr(expert_data.implies_arr)
 
-    for key, val in statement.Statements.items():
+    for key, val in StatementMap.Statements.items():
         print(f'key: {key}, val {val.value}')
+
+    resolve_queries(expert_data.queries)
+
+    
 
 
--- statement.py
+++ statement.py
@@ -1,7 +1,9 @@
 
 # key is a fact name ( A, D, C ...)
 # value is a StatementValue class
-Statements = dict()
+class StatementMap:
+    Statements = dict()
+
 
 class StatementValue:
     #can containe:
@@ -12,3 +14,39 @@ class StatementValue:
 
     def is_computed(self):
         return self.value is bool
+
+
+def init_from_facts(initial_facts):
+    #is seams to '=ABC'?
+    if not isinstance(initial_facts, str) or initial_facts[0] != '=':
+        raise Exception('wrong format of initial facts')
+
+    elif len(initial_facts) > 1:
+        for fact in initial_facts[1:]:
+            StatementMap.Statements[fact] = StatementValue(True)
+
+
+def init_form_initial_facts_arr(initial_facts_arr):
+    for initial_facts in initial_facts_arr:
+        init_from_facts(initial_facts)
+
+
+def fromate_implies(implies):
+    implies_parts = str(implies).partition('=>')
+    #is implies seams to 'C => E'?
+    if not isinstance(implies, str) or len(implies_parts) != 3:
+        raise Exception('wrong format of implies')
+    elif len(implies_parts[2]) == 0 or len(implies_parts[0]) == 0:
+        raise Exception('some part of implies is empty')
+    elif '<' in implies_parts[0]:
+        raise Exception('does not work with biconditional rules')
+
+    return implies_parts
+
+
+def init_from_implies_arr(implies_arr):
+    for implies in implies_arr:
+        implies_parts = fromate_implies(implies)
+        # 'E' became a key and 'C' become a value
+        # implies_parts ~ {'C', '=>', 'E'}.
+        StatementMap.Statements[implies_parts[2]] = StatementValue(implies_parts[0])
