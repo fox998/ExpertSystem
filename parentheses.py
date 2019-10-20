f8d193ff2b69162070be1fd2056e27cf1e257856
Sergienko Tanya <chocolate1378@gmail.com>
2019-10-20 13:21:27 +0300

    operations priority

--- parentheses.py
+++ parentheses.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+
+from functools import reduce
+
+
 def get_next_item(s: str):
     '''Used in split. Returns next operation(^+|!) , fact(A, B...) or term ((A+B)|C)...'''
     if s[0] in '!+|^' or s[0] != '(':
@@ -16,6 +20,23 @@ def get_next_item(s: str):
     return [len(s[:end_index+1]), s[:end_index+1]]
 
 
+# operations order: NOT > XOR > AND > OR 
+def check_operations_order(stack: list) -> list:
+    priority_dict = {'^': 1, '+': 2, '|': 3}
+    operations_list = [el for el in stack if el in '+|^']
+    order = [priority_dict[el] for el in stack if el in '+|^']
+    if sorted(order) == order:
+        return stack
+
+    corrected_stack = []
+    operation = operations_list[order.index(min(order))]
+    ind = stack.index(operation)
+    corrected_stack.append( reduce(lambda x1, x2: x1+x2, stack[:ind]) )
+    corrected_stack.append(operation)
+    corrected_stack.append( reduce(lambda x1, x2: x1+x2, stack[ind+1:]) )
+    return corrected_stack
+
+
 def split_terms(term: str) -> list:
     '''returns list like [A, +, (!B+(D+E)), |, F]'''
     stack = []
@@ -30,6 +51,7 @@ def split_terms(term: str) -> list:
         else:
             stack.append(item)
         i = i + shift
+    stack = check_operations_order(stack)
     return stack
 
 
@@ -46,13 +68,18 @@ def check_parentheses_order(term: str):
 
 
 def test():
-    s = '!A+(B|C)'
+    s = 'A|B+C'
     print(f'{s} = {split_terms(s)}')
+    print(f'{s} = {check_operations_order(s)}')
     s = '!A+B'
     print(f'{s} = {split_terms(s)}')
     s = 'A+!B'
     print(f'{s} = {split_terms(s)}')
     s = '!A'
+    print(f'{s} = {split_terms(s)}')
+    s = 'A^B+C|D'
+    print(f'{s} = {split_terms(s)}')
+    s = '!A^!B|!D+!E^U'
     print(f'{s} = {split_terms(s)}')
 
 
